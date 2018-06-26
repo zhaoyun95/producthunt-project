61b942fa987980326a41f158afbc946c7b44c1f5
James Ye <yiteng@yahoo.com>
2018-06-25 21:14:33 -0700

    Initial Commit

---
+++ products/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.TextField()
+    pub_date = models.DateTimeField()
+    votes_total = models.IntegerField(default=1)
+    image = models.ImageField(upload_to='images/')
+    icon = models.ImageField(upload_to='images/')
+    body = models.TextField()
+    pub_date_pretty = models.DateField()
+    # store the User id in this table.  Delete his products if the user is deleted
+    hunter = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
